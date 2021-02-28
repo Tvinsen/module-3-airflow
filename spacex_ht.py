88712037a114f8eb1536e5ef7e23b69a83f12412
Tvinsen <79773123+Tvinsen@users.noreply.github.com>
2021-02-28 09:27:22 +0700

    Update spacex_ht.py

--- spacex_ht.py
+++ spacex_ht.py
@@ -18,7 +18,7 @@ dag = DAG("spacex_ht", default_args=default_args, schedule_interval="0 0 1 1 *")
 
 t1 = BashOperator(
     task_id="get_data", 
-    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data", 
+    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data_ht", 
     dag=dag
 )
 
