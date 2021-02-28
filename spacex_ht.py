4153df2f9711d45b79243498c8631116ba9fd91b
Tvinsen <79773123+Tvinsen@users.noreply.github.com>
2021-02-28 11:53:31 +0700

    Update spacex_ht.py

--- spacex_ht.py
+++ spacex_ht.py
@@ -16,18 +16,18 @@ default_args = {
 
 dag = DAG("spacex_ht", default_args=default_args, schedule_interval="0 0 1 1 *")
 spacexrockets = ['falcon1', 'falcon9', 'falconheavy', 'all']
-for i in spacexrockets:
-    t1 = BashOperator(
-        task_id="get_data", 
-        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -r i -o /var/data_ht", 
-        dag=dag
-    )
 
-for i in spacexrockets:
+t1 = BashOperator(
+    task_id="get_data",
+    for i in spacexrockets:
+        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -r i -o /var/data_ht", 
+    dag=dag
+)
+
     t2 = BashOperator(
         task_id="print_data", 
         bash_command="cat /var/data_ht/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
-        params={"rocket": (i) }, # falcon1/falcon9/falconheavy
+        params={"rocket": all }, # falcon1/falcon9/falconheavy
         dag=dag
     )
 t1 >> t2
