b2419974469e7e0ef1a743e02db518296dc0d2fb
Tvinsen <79773123+Tvinsen@users.noreply.github.com>
2021-02-28 11:01:41 +0700

    Update spacex_ht.py

--- spacex_ht.py
+++ spacex_ht.py
@@ -26,7 +26,7 @@ for i in spacexrockets:
     t2 = BashOperator(
         task_id="print_data", 
         bash_command="cat /var/data_ht/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
-        params={"rocket": (i)}, # falcon1/falcon9/falconheavy
+        params={"rocket": str(i)}, # falcon1/falcon9/falconheavy
         dag=dag
     )
 
